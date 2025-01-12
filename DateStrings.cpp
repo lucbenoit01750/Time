91d8b10186917de66fd6fbece27e4abaecda5334
ricaharina <luc.benoit@sfr.fr>
2025-01-12 12:13:16 +0100

    Translated strings in french and added weak attribute to functions

--- DateStrings.cpp
+++ DateStrings.cpp
@@ -7,6 +7,7 @@
  * You can change the text of the strings, make sure the short strings are each exactly 3 characters 
  * the long strings can be any length up to the constant dt_MAX_STRING_LEN defined in TimeLib.h
  * 
+ 2025-01-12 : ChipiDev.LBT : Translated labels in french; added weak to functions.
  */
 
 #include <Arduino.h>
@@ -18,9 +19,6 @@
 #undef PROGMEM
 #define PROGMEM
 #endif
-
-#include "TimeLib.h"
-
  
 // the short strings for each day or month must be exactly dt_SHORT_STR_LEN
 #define dt_SHORT_STR_LEN  3 // the length of short strings
@@ -28,18 +26,18 @@
 static char buffer[dt_MAX_STRING_LEN+1];  // must be big enough for longest string and the terminating null
 
 const char monthStr0[] PROGMEM = "";
-const char monthStr1[] PROGMEM = "January";
-const char monthStr2[] PROGMEM = "February";
-const char monthStr3[] PROGMEM = "March";
-const char monthStr4[] PROGMEM = "April";
-const char monthStr5[] PROGMEM = "May";
-const char monthStr6[] PROGMEM = "June";
-const char monthStr7[] PROGMEM = "July";
-const char monthStr8[] PROGMEM = "August";
-const char monthStr9[] PROGMEM = "September";
-const char monthStr10[] PROGMEM = "October";
-const char monthStr11[] PROGMEM = "November";
-const char monthStr12[] PROGMEM = "December";
+const char monthStr1[] PROGMEM = "Janvier";
+const char monthStr2[] PROGMEM = "Février";
+const char monthStr3[] PROGMEM = "Mars";
+const char monthStr4[] PROGMEM = "Avril";
+const char monthStr5[] PROGMEM = "Mai";
+const char monthStr6[] PROGMEM = "Juin";
+const char monthStr7[] PROGMEM = "Juillet";
+const char monthStr8[] PROGMEM = "Août";
+const char monthStr9[] PROGMEM = "Septembre";
+const char monthStr10[] PROGMEM = "Octobre";
+const char monthStr11[] PROGMEM = "Novembre";
+const char monthStr12[] PROGMEM = "Décembre";
 
 const PROGMEM char * const PROGMEM monthNames_P[] =
 {
@@ -50,30 +48,31 @@ const PROGMEM char * const PROGMEM monthNames_P[] =
 const char monthShortNames_P[] PROGMEM = "ErrJanFebMarAprMayJunJulAugSepOctNovDec";
 
 const char dayStr0[] PROGMEM = "Err";
-const char dayStr1[] PROGMEM = "Sunday";
-const char dayStr2[] PROGMEM = "Monday";
-const char dayStr3[] PROGMEM = "Tuesday";
-const char dayStr4[] PROGMEM = "Wednesday";
-const char dayStr5[] PROGMEM = "Thursday";
-const char dayStr6[] PROGMEM = "Friday";
-const char dayStr7[] PROGMEM = "Saturday";
+const char dayStr1[] PROGMEM = "Dimanche";
+const char dayStr2[] PROGMEM = "Lundi";
+const char dayStr3[] PROGMEM = "Mardi";
+const char dayStr4[] PROGMEM = "Mercredi";
+const char dayStr5[] PROGMEM = "Jeudi";
+const char dayStr6[] PROGMEM = "Vendredi";
+const char dayStr7[] PROGMEM = "Samedi";
 
 const PROGMEM char * const PROGMEM dayNames_P[] =
 {
    dayStr0,dayStr1,dayStr2,dayStr3,dayStr4,dayStr5,dayStr6,dayStr7
 };
 
-const char dayShortNames_P[] PROGMEM = "ErrSunMonTueWedThuFriSat";
+const char dayShortNames_P[] PROGMEM = "Errdimlunmarmerjeuvensam";
 
 /* functions to return date strings */
-
-char* monthStr(uint8_t month)
+/* LBT : added __attribute__((weak)) */
+__attribute__((weak)) char* monthStr(uint8_t month)
 {
     strcpy_P(buffer, (PGM_P)pgm_read_ptr(&(monthNames_P[month])));
     return buffer;
 }
 
-char* monthShortStr(uint8_t month)
+
+__attribute__((weak)) char* monthShortStr(uint8_t month)
 {
    for (int i=0; i < dt_SHORT_STR_LEN; i++)      
       buffer[i] = pgm_read_byte(&(monthShortNames_P[i+ (month*dt_SHORT_STR_LEN)]));  
@@ -81,13 +80,13 @@ char* monthShortStr(uint8_t month)
    return buffer;
 }
 
-char* dayStr(uint8_t day) 
+__attribute__((weak)) char* dayStr(uint8_t day) 
 {
    strcpy_P(buffer, (PGM_P)pgm_read_ptr(&(dayNames_P[day])));
    return buffer;
 }
 
-char* dayShortStr(uint8_t day) 
+__attribute__((weak)) char* dayShortStr(uint8_t day)  
 {
    uint8_t index = day*dt_SHORT_STR_LEN;
    for (int i=0; i < dt_SHORT_STR_LEN; i++)      
